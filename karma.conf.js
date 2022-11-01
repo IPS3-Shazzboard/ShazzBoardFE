58e6ff32cbc5f12beb0ef4450b052d3d32413c07
Calvin Zhen <418224@student.fontys.nl>
2022-11-01 13:38:56 +0100

    Added cards

--- karma.conf.js
+++ karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Firefox"],
+    browsers: ["Firefox", "FirefoxDeveloper"],
     singleRun: false,
     restartOnFileChange: true,
   });
